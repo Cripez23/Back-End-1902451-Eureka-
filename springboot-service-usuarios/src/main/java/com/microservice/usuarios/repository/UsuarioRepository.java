23f731c3dba91c93ffc2e83917cdda9687daa00c
Cripez23 <cris_yoo23@hotmail.com>
2023-11-26 12:02:51 -0600

    RepositoryRestResource HATEOAS

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
@@ -2,18 +2,20 @@ package com.microservice.usuarios.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.microservice.usuarios.entity.Usuario;
 
+@RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long>{
 	
-	//select u from Usuario u where u.ysername = ?1
+	//select u from Usuario u where u.username = ?1
 	public Usuario findByUsername(String username);
 	
 	//select u from Usuario u where u.username = ?1 and u.email = ?2
 	public Usuario findByUsernameAndEmail(String username, String email);
 	
-	@Query(value = "select u from Usuario u where u.ysername = ?1")
+	@Query(value = "select u from Usuario u where u.username = ?1")
 	public Usuario obtenerPorUsername(String username);
 	
 	@Query(value = "select u from Usuario u where u.username = ?1 and u.email = ?2")
---
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Cris', '12345', 1, 'Cristopher', 'Perez', 'cris_yoo23@hotmail.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Emma', '12345', 1, 'Emmanuel', 'Baltasar', 'Emma23@hotmail.com');
