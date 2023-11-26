334125a9bd63ac967fdf71140ae2f6f56a44586f
Cripez23 <cris_yoo23@hotmail.com>
2023-11-26 13:04:26 -0600

    RestResource Personalizado

--- springboot-service-usuarios/src/main/resources/import.sql
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -1,2 +1,9 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Cris', '12345', 1, 'Cristopher', 'Perez', 'cris_yoo23@hotmail.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Cris', '12345', 1, 'Cristopher', 'Perez', 'cris_yoo23@hotmail.com');
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Emma', '12345', 1, 'Emmanuel', 'Baltasar', 'Emma23@hotmail.com');
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES (1, 2);
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES (2, 2);
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES (2, 1);
